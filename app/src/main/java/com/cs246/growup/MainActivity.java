248d8cad35c5bd4313a760fa3fb29815a0ae0df2
shadolite <amy.kawaii@gmail.com>
2020-02-29 11:22:47 -0700

    Committed initial project files.

---
+++ app/src/main/java/com/cs246/growup/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cs246.growup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
