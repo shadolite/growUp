e1af87a9cea7cf437394e91398caa10d254fad2b
animacpac <56101332+animacpac@users.noreply.github.com>
2020-03-05 20:15:24 -0700

    for some reason openFileOutput is red, any ideas? https://developer.android.com/reference/android/content/Context#openFileOutput(java.lang.String,%20int)

--- app/src/main/java/com/cs246/growup/Models/Storage.java
+++ app/src/main/java/com/cs246/growup/Models/Storage.java
@@ -1,9 +1,37 @@
 package com.cs246.growup.Models;
 
+import java.io.FileOutputStream;
+import android.content.Context;
+import com.google.gson.Gson;
+import java.io.FileInputStream;
+
 public class Storage {
     String Entry;
     String Goals;
     public void createFile(){}
     public void readFile(){}
-    public void writeFile(){}
+
+    public Storage(String entry) {
+        Entry = entry;
+    }
+
+    public void writeFile(String goalEntry){
+        String filename = "myfile.txt";
+
+
+        Gson gson = new Gson();
+        String s = gson.toJson(goalEntry);
+
+        FileOutputStream outputStream;
+
+        try {
+
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(s.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }}
+
+
 }
