734de8bb8d128fe2ef46863c9045a0fba7144061
animacpac <56101332+animacpac@users.noreply.github.com>
2020-03-06 21:52:48 -0700

    Storage update.

--- app/src/main/java/com/cs246/growup/Models/Storage.java
+++ app/src/main/java/com/cs246/growup/Models/Storage.java
@@ -26,6 +26,7 @@ public class Storage {
             outputStream.write(s.getBytes());
             outputStream.close();
 
+            
 
         } catch (Exception e) {
             e.printStackTrace();
