b30f7310f10990802b3da853eb744f1b47be5223
CCox7 <60279824+CCox7@users.noreply.github.com>
2020-03-14 00:08:18 -0600

    Browse Fragment update, but not complete, suggestions may help.

--- app/src/main/java/com/cs246/growup/Views/BrowseFragment.java
+++ app/src/main/java/com/cs246/growup/Views/BrowseFragment.java
@@ -1,21 +1,40 @@
 package com.cs246.growup.Views;
 
-public class BrowseFragment {
+import android.os.Bundle;
 
-    public void onCreate() {
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.cs246.growup.Models.Config;
+import com.cs246.growup.Models.Entry;
+import com.cs246.growup.Models.Goal;
+import com.cs246.growup.Presenters.Listener;
+
+import java.util.List;
+
+
+public class BrowseFragment extends AppCompatActivity implements Listener {
+
+    public BrowseFragment() {};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
     }
     public void onCreateView() {
 
     }
     public void onStop() {
-
-    }
-    public void notifyDataReady() {
-
+        super.onStop();
     }
 
+    @Override
+    public void notifyDataReady(Config config, List<Goal> goals, Entry entry) {
 
+    }
 
+    @Override
+    public void NotifyConfigChanged() {
 
+    }
 }
