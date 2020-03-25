d3790a5248adfe537133528d5a6a5a7ff7d25743
animacpac <56101332+animacpac@users.noreply.github.com>
2020-03-24 19:29:41 -0600

    SearchResultsView Layout updated

--- app/src/main/java/com/cs246/growup/Views/SearchFragment.java
+++ app/src/main/java/com/cs246/growup/Views/SearchFragment.java
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment;
 import com.cs246.growup.Models.Config;
 import com.cs246.growup.Models.Entry;
 import com.cs246.growup.Models.Goal;
+import com.cs246.growup.Models.User;
 import com.cs246.growup.Presenters.Listener;
 
 import java.util.List;
@@ -16,13 +17,23 @@ public class SearchFragment extends Fragment implements Listener {
     public void notifyDataReady() {}
     private void updateSearchResults() {}
 
-    @Override
+   // @Override
     public void notifyDataReady(Config config, List<Goal> goals, Entry entry) {
 
     }
 
-    @Override
+    //@Override
     public void NotifyConfigChanged() {
+
+    }
+
+    @Override
+    public void notifyDataReady(User user, Config config) {
+
+    }
+
+    @Override
+    public void notifyConfigChanged() {
 
     }
 }
