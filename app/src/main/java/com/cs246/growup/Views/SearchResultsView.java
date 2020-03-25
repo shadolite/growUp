fe4bce90b12cfad018828277234ef8c9f8afb95f
animacpac <56101332+animacpac@users.noreply.github.com>
2020-03-24 19:28:03 -0600

    SearchResultsView Layout updated

--- app/src/main/java/com/cs246/growup/Views/SearchResultsView.java
+++ app/src/main/java/com/cs246/growup/Views/SearchResultsView.java
@@ -1,24 +1,10 @@
 package com.cs246.growup.Views;
 
-import android.os.Bundle;
-import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.cs246.growup.R;
 
-public class SearchResultsView extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle searched) {
-        super.onCreate(searched);
-        setContentView(R.layout.activity_search_view);
-    }
+public class SearchResultsView{
 
-
-    //only needed if we need to override the onStop() method included with AppCompatActivity
-    public void onStop() {
-        super.onStop();
-    }
-    public void notifyDataReady() {}
-    public void notifyConfigChanged() {}
 
 }
